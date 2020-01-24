4114cfa6611adaddd4f1703e55d7e4be8be04c32
Aleksandra Ustymenko <oleksandraustymenko@gmail.com>
2020-01-24 20:20:09 +0100

    Poprawka

--- FitClub(GUI)/NewAktywnosc.cs
+++ FitClub(GUI)/NewAktywnosc.cs
@@ -27,11 +27,11 @@ namespace FitClub_GUI_
         private void button1_Click(object sender, EventArgs e)
         {
           
-            //if(textBox1.Text.Length==0)
-            //{
-            //    MessageBox.Show("Uzupelnij nazwe","nazwa jest wymagana",MessageBoxButtons.OK,MessageBoxIcon.Error);
-            //    return;
-            //}
+            if(textBox1.Text.Length==0)
+            {
+                MessageBox.Show("Uzupelnij nazwe","nazwa jest wymagana",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                return;
+            }
             Nazwa = textBox1.Text;
             wartosc=numericUpDown1.Value;
             this.Close();
